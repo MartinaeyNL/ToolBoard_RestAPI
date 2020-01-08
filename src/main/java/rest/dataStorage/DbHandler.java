f05e001ee2c94fe60bdacb5efe4bb4d86e834294
Martin Peeters <406336@student.fontys.nl>
2020-01-08 16:21:01 +0100

    Removed some uncommented code

--- src/main/java/rest/dataStorage/DbHandler.java
+++ src/main/java/rest/dataStorage/DbHandler.java
@@ -23,18 +23,4 @@ public class DbHandler {
         if(toReturn != null) { return toReturn; }
         return new ArrayList<>();
     }
-        //Collection<JsonObject> toReturn = new ArrayList<>();
-        //try {
-        //    Statement statement = this.dataConHandler.executeQuery("SELECT * FROM `Users`");
-        //    if(set.next() == true) {
-        //        JsonObject obj = new JsonObject();
-        //        obj.addProperty("displayname", set.getString(1));
-        //        obj.addProperty("twitchToken", set.getString(2));
-        //        toReturn.add(obj);
-        //    }
-        //    if(toReturn.size() > 0) { return toReturn; }
-        //    return null;
-        //}
-        //catch (SQLException ex) { ex.printStackTrace(); }
-        //return toReturn;
 }
